e6f4a108d401fce2862218b5694269ac00e6829c
rebecafc93 <rebecafc@alu.ufc.br>
2025-07-01 21:07:06 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,108 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01",
+      "name": "01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.5489062786582437,
+        "pitch": -0.027923589076090494,
+        "fov": 1.2733156773635639
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5753301026336004,
+          "pitch": 0.09513598885765973,
+          "rotation": 0,
+          "target": "1-06"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-06",
+      "name": "06",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.5418677580889941,
+        "pitch": 0.0007181183650732947,
+        "fov": 1.2733156773635639
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7401090902222265,
+          "pitch": 0.20336272795674049,
+          "rotation": 0,
+          "target": "0-01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.184959593308145,
+          "pitch": -0.30457118390513926,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-family: Helvetica, sans-serif;\">Cauço (<i>Ficus lutea</i>)</span><span style=\"background: white;\"><o:p></o:p></span></p>",
+          "text": "<p class=\"MsoNormal\" style=\"text-align:justify\">Também conhecida como belaque, o\ncauço é uma espécie nativa da África, pertencente à família Moraceae. Trata-se\nde uma árvore de grande porte, que pode atingir até 25 metros de altura, com\ncopa ampla, chegando a 30 a 45 metros de diâmetro, o que a torna excelente para\narborização de áreas amplas, como praças, grandes jardins e canteiros urbanos,\nquando plantada de forma isolada e sem obstáculos ao redor. Por conta do porte\ne da expansão de sua copa, não é recomendada para calçadas estreitas ou sob\nredes elétricas.<o:p></o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\">As flores da <i>Ficus lutea</i>\nsão pequenas e dispostas em cachos entre o caule e as folhas. Os frutos são\ndrupas pequenas e carnosas, que adquirem coloração amarela a alaranjada quando\nmaduros, sendo comestíveis em algumas regiões da África Ocidental.<o:p></o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\">Além do uso alimentar, a espécie\npossui importância medicinal e utilitária: os frutos são utilizados\ntradicionalmente em Omã para auxiliar na cicatrização de feridas e no\ntratamento de furúnculos, enquanto as folhas jovens, especialmente na região\nocidental de Camarões, são empregadas no tratamento de diarreias. Já em Angola,\nsua madeira é aproveitada na confecção de utensílios domésticos, como tigelas.<o:p></o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><span lang=\"EN-US\">Referências:<o:p></o:p></span></p><p class=\"MsoNormal\" style=\"text-align:justify\"><span lang=\"EN-US\">&nbsp;</span></p><p class=\"MsoNormal\" style=\"text-align:justify\"><span lang=\"EN-US\">1. <b>SONFACK FOZENG, Herman D. et al.</b> A\nnew ceramide and other constituents from the fruits of Ficus lutea Vahl\n(Moraceae) and their chemotaxonomic significance. </span>Biochemical\nSystematics and Ecology, Amsterdam, v. 106, e104573, 2023. Disponível em: https://doi.org/10.1016/j.bse.2022.104573.<o:p></o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\">\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n</p><p class=\"MsoNormal\" style=\"text-align:justify\">2. <b>BIOLOGIA DA PAISAGEM.</b>\nFicus lutea – belaque. Biologia da Paisagem, 17 jan. 2023. Disponível em: &lt;https://biologiadapaisagem.com.br/2023/01/17/ficus-lutea-belaque/&gt;.\nAcesso em: 24 jun. 2025.<o:p></o:p></p>"
+        },
+        {
+          "yaw": -0.1972370884196959,
+          "pitch": -0.16481462379780254,
+          "title": "<p class=\"MsoNormal\">Aroeira (<i>Myracrodruon urundeuva</i>)<o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\" style=\"text-align:justify\">A aroeira é uma espécie arbórea\nnativa do Ceará, pertencente à família Anacardiaceae. Embora seja nativa do\nCeará, não é endêmica do Brasil, ocorrendo amplamente no continente americano,\ndesde o México até a Argentina.<o:p></o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\">A árvore pode atingir entre 8 e\n20 metros de altura, podendo chegar a 30 metros em condições ideais. No Ceará,\na floração acontece entre os meses de março e abril, e os frutos maduros\naparecem em setembro. As flores são principalmente visitadas por himenópteros,\ndestacando-se as abelhas, que atuam como seus principais polinizadores.&nbsp;<o:p></o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><b>Uso econômico e medicinal<o:p></o:p></b></p><p class=\"MsoNormal\" style=\"text-align:justify\">A madeira da aroeira é muito\nvalorizada por suas boas qualidades físico-mecânicas, sendo amplamente\nutilizada na produção de obras externas, estacas, vigas, ripas, caibros, tacos\npara assoalho, além de ser fonte de lenha e carvão.<o:p></o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\">As cascas e resinas da planta são\nricas em fenóis e taninos, o que justifica seu uso tradicional na medicina\npopular. São empregadas como adstringentes, balsâmicas, analgésicas,\ncicatrizantes, anti-inflamatórias, antibacterianas e hemostáticas. Seu uso\ninclui tratamentos para inflamações da garganta, gengiva e pele, além de\nenfermidades genitais e urinárias, problemas das vias respiratórias, gastrites,\núlceras estomacais, resfriados, diarreia, reumatismo e até para a regulação do\nciclo menstrual.<o:p></o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\">Além disso, as cascas são uma\nimportante fonte de taninos para o curtimento de peles, e as folhas fornecem\nmaterial corante utilizado no tingimento de tecidos. As folhas também servem como\nalimento para o gado.<o:p></o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><b>Aplicações ambientais e\npaisagísticas<o:p></o:p></b></p><p class=\"MsoNormal\" style=\"text-align:justify\">Devido à sua arquitetura e porte,\na aroeira é muito utilizada na arborização urbana. É recomendada para a\nrecuperação de solos degradados e compactos, sendo ideal seu plantio em\nconsórcio com gramíneas para melhor recuperação do solo.<o:p></o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><i>*Encontra-se na lista de\nespécies nativas recomendadas para ações de florestamento e reflorestamento no\nEstado do Ceará. (Instrução normativa Nº05/2021).<o:p></o:p></i></p><p class=\"MsoNormal\" style=\"text-align:justify\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\">Referências:<o:p></o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\">1. <b>PAREYN, F. G. C.; ARAÚJO, E. de L.; DRUMOND, M. A.;\nMIRANDA, M. J. de A. C.; SOUZA, C. A.; SILVA, A. P. de S.; BRAZOLIN, S.;\nMARQUES, K. K. M.</b> <i>Myracrodruon urundeuva</i>: Aroeira. In: CORADIN, L.;\nCAMILLO, J.; PAREYN, F. G. C. (Coords.). Espécies nativas da flora brasileira\nde valor econômico atual ou potencial: plantas para o futuro: região Nordeste.\nBrasília, DF: MMA; 2018. (Série Biodiversidade, n. 51), cap. 5,\np. 766–772.&nbsp;<o:p></o:p></p><p class=\"MsoNormal\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\">2. <b>ÁRVORES DO BIOMA CERRADO.</b> <i>Astronium urundeuva\n(M. Allemão) Engl.</i> Sinônimo: Myracrodruon urundeuva M. Allemão. Publicado\nem 30 mar. 2017. Disponível em: &lt;https://www.arvoresdobiomacerrado.com.br/site/2017/03/30/myracrodruon-urundeuva-allemao/&gt;.\nAcesso em: 24 jun. 2025.<o:p></o:p></p><p class=\"MsoNormal\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\"><o:p>&nbsp;</o:p></p><p class=\"MsoNormal\" style=\"text-align:justify\">\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n</p><p class=\"MsoNormal\">3. Aroeira-Verdadeira: <i>Myracrodruon urundeuva</i>. In:\nCARVALHO, P. E. R. Espécies arbóreas brasileiras. Brasília: Embrapa Informação\nTecnológica; Colombo: Embrapa Florestas, 2003. v. 1, p. 177-1875.<o:p></o:p></p>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
